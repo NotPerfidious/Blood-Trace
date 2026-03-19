87fabe351dbf350076ff37d59098dc4e8f11ddc2
Muhammad Basim Irfan <mbasimirfan65@gmail.com>
2026-03-19 19:26:10 +0500

    completed frontend setup

---
+++ Blood-Trace/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-10xl font-bold flex justify-center'>Blood Trace Project</div>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-2xl font-bold text-red-600">
+          Tailwind is Working
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
